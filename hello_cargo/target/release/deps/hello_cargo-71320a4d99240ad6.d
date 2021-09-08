0c2e2df6fb1dfdf9da85f6d4eb9b6e8e0a78e007
daniniguez <daniel.iniguez.villa@gmail.com>
2021-09-07 21:20:39 -0500

    Build release

---
+++ hello_cargo/target/release/deps/hello_cargo-71320a4d99240ad6.d
@@ -0,0 +1,5 @@
+/home/dan/documents/rust/learning-rust/hello_cargo/target/release/deps/hello_cargo-71320a4d99240ad6: src/main.rs
+
+/home/dan/documents/rust/learning-rust/hello_cargo/target/release/deps/hello_cargo-71320a4d99240ad6.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dan/documents/rust/learning-rust/hello_cargo/target/release/hello_cargo: /home/dan/documents/rust/learning-rust/hello_cargo/src/main.rs
