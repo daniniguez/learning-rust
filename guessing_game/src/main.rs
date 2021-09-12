380c1cb9e14c51738641446598a066f33fa3c89e
daniniguez <daniel.iniguez.villa@gmail.com>
2021-09-11 21:10:24 -0500

    First part of the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,59 @@
+//Module that contains Read and Write traits
+//including the ability to accept user input.
+use std::io; 
+
 fn main() {
-    println!("Hello, world!");
+
+    //Printing a prompt stating what the game is
+    //and requesting input from the user.
+    println!("Guess the number!");
+    
+    println!("Please input your guess.");
+
+    //let creates a variable named guess to store
+    //the user input.
+    //mut makes a mutable variable.
+
+    //String::new is a function that returns a
+    //new instance of a 'String'
+    //'String' is a type provided by the standard library.
+    //:: syntax in ::new indicates that new is an
+    //associated function of the 'String' type.
+    //it creates a new empty string.
+    let mut guess = String::new();
+
+    //stdin is a type that represents a handle
+    //to the standard input of the terminal.
+    
+    //read_line is a method that gets the input
+    //from the user from the standard input handle.
+
+    //We are passing the &mut guess argument.
+    //read_line takes what the user types into the
+    //standard input and place that into a string
+    //using that string as an argument and it needs
+    //to be 'mut'able.
+
+    //& indicates that this argument is a reference
+    //making data available without needing to
+    //copy that data into the memory multiple times.
+    io::stdin().read_line(&mut guess)
+
+        //read_line returns a value io::Result
+        //'Result' type is an enumeration which is
+        //type that can have a fixed set of values.
+
+        //For 'Result' the variants are 'Ok' and 'Err'.
+        //This to encode error handling information.
+        //As any other type it has methods defined on it.
+        //expect is a method for io::Result.
+
+        //If the instance for io:Result is 'Err' expect
+        //will cause the program to crash and display
+        //the message passed as an argument.
+        .expect("Failed to read line");
+
+    //Prints the sting we saved the user's input in.
+    //{} is a placeholder that will print guess' value.
+    println!("You guessed: {}", guess);
 }
