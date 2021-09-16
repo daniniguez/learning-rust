3347b5853f06cf60cf8f4bc90fc7b0803b5bd341
daniniguez <daniel.iniguez.villa@gmail.com>
2021-09-15 20:45:31 -0500

    Added loop to allow more guess chances

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -31,66 +31,74 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    //Inserting a loop to give the user more chances
+    //to guess.
+    loop {
+        println!("Please input your guess.");
 
-    //let creates a variable named guess to store
-    //the user input.
-    //mut makes a mutable variable.
-    //String::new is a function that returns a
-    //new instance of a 'String'
-    //'String' is a type provided by the standard library.
-    //:: syntax in ::new indicates that new is an
-    //associated function of the 'String' type.
-    //it creates a new empty string.
-    let mut guess = String::new();
+        //let creates a variable named guess to store
+        //the user input.
+        //mut makes a mutable variable.
+        //String::new is a function that returns a
+        //new instance of a 'String'
+        //'String' is a type provided by the standard library.
+        //:: syntax in ::new indicates that new is an
+        //associated function of the 'String' type.
+        //it creates a new empty string.
+        let mut guess = String::new();
 
-    //stdin is a type that represents a handle
-    //to the standard input of the terminal.    
-    //read_line is a method that gets the input
-    //from the user from the standard input handle.
-    //We are passing the &mut guess argument.
-    //read_line takes what the user types into the
-    //standard input and place that into a string
-    //using that string as an argument and it needs
-    //to be mutable.
-    //& indicates that this argument is a reference
-    //making data available without needing to
-    //copy that data into the memory multiple times.
-    io::stdin().read_line(&mut guess)
+        //stdin is a type that represents a handle
+        //to the standard input of the terminal.    
+        //read_line is a method that gets the input
+        //from the user from the standard input handle.
+        //We are passing the &mut guess argument.
+        //read_line takes what the user types into the
+        //standard input and place that into a string
+        //using that string as an argument and it needs
+        //to be mutable.
+        //& indicates that this argument is a reference
+        //making data available without needing to
+        //copy that data into the memory multiple times.
+        io::stdin().read_line(&mut guess)
 
-        //read_line returns a value io::Result
-        //'Result' type is an enumeration which is
-        //type that can have a fixed set of values.
-        //For 'Result' the variants are 'Ok' and 'Err'.
-        //This to encode error handling information.
-        //As any other type it has methods defined on it.
-        //expect is a method for io::Result.
-        //If the instance for io:Result is 'Err' expect
-        //will cause the program to crash and display
-        //the message passed as an argument.
-        .expect("Failed to read line");
+            //read_line returns a value io::Result
+            //'Result' type is an enumeration which is
+            //type that can have a fixed set of values.
+            //For 'Result' the variants are 'Ok' and 'Err'.
+            //This to encode error handling information.
+            //As any other type it has methods defined on it.
+            //expect is a method for io::Result.
+            //If the instance for io:Result is 'Err' expect
+            //will cause the program to crash and display
+            //the message passed as an argument.
+            .expect("Failed to read line");
 
-    //Here let guess declares a new variable that replaces
-    //guess string variable declared before
-    //by means of calling the trim method which
-    //removes any blank spaces at the end or beginning
-    //of the string and the parse method parsing the string
-    //into some kind of number in this case a u32 integer.
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        //Here let guess declares a new variable that replaces
+        //guess string variable declared before
+        //by means of calling the trim method which
+        //removes any blank spaces at the end or beginning
+        //of the string and the parse method parsing the string
+        //into some kind of number in this case a u32 integer.
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    //Prints the string we saved the user's input in.
-    //{} is a placeholder that will print guess value.
-    println!("You guessed: {}", guess);
+        //Prints the string we saved the user's input in.
+        //{} is a placeholder that will print guess value.
+        println!("You guessed: {}", guess);
 
-    //cmp method compares 2 values guess and the secret number.
-    //match expression is made up of 'arms'. An arm consist
-    //of a pattern and the code that should be run if the
-    //value given to the beginning of the match expression
-    //fits the arm's pattern.
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        //cmp method compares 2 values guess and the secret number.
+        //match expression is made up of 'arms'. An arm consist
+        //of a pattern and the code that should be run if the
+        //value given to the beginning of the match expression
+        //fits the arm's pattern.
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                //Exit the loop when the guess is correct.
+                break;
+            }
+        }
     }
 }
